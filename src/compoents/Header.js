bb24be06be7e11e6977f7932414e30ecad4db79a
rsbassi9 <rajvir.bassi95@gmail.com>
2023-06-08 05:37:52 -0700

    Fixed typo for punkLogo, added search bar

--- src/compoents/Header.js
+++ src/compoents/Header.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import './Header.css'
 import punkLogo from '../assets/header/cryptopunk-logo.png'
+import searchIcon from '../assets/header/search.png'
 
 const Header = () => {
   return (
     <div className='header'>
     <div className='logoContainer'>
-        <img src={punkLogo} clasName='punkLogo' alt=''/>
+        <img src={punkLogo} className='punkLogo' alt=''/>
+    </div>
+    <div className='searchBar'>
+        <div className='searchIconContainer'>
+            <img src={searchIcon} alt=''/>
+        </div>
+        <input className='searchInput' />
     </div>
     </div>
   )
